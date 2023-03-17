5c342bd1b5688719df161d1e43d0a0c2f5b2872a
davidrk <david_knotten@hotmail.no>
2023-03-17 15:19:43 +0100

    Hex implementation completed

--- hex/hexWorld.py
+++ hex/hexWorld.py
@@ -1,8 +1,9 @@
 import copy
 
 import numpy as np
-from typing import TypeVar
+from typing import TypeVar, Callable
 from game import Game
+from queue import LifoQueue
 
 HexWorld = TypeVar("HexWorld", bound="HexWorld")
 
@@ -32,11 +33,11 @@ class HexWorld(Game):
                 actions.append(self.pad(i)+self.pad(j))
         return actions
 
-    def apply(self, action: str) -> Game:
+    def apply(self, action: str, deepcopy: bool = False) -> HexWorld:
         coordinates = (int(action[:2]), int(action[-2:]))
         if coordinates[0] >= self.size or coordinates[1] >= self.size:
             raise Exception(f"Action {action} cannot be applied, as the coordinate is out of bounds")
-        tmp = copy.deepcopy(self)
+        tmp = copy.deepcopy(self) if deepcopy else self
         piece = tmp.board[coordinates[0]][coordinates[1]]
         if piece[0] != 0 or piece[1] != 0:
             raise Exception('Cannot place piece on an occupied coordinate')
@@ -56,6 +57,61 @@ class HexWorld(Game):
         padding = "0"*length
         out = padding+str(input) if start else str(input)+padding
         return out[-length:]
+
+    def is_final_state(self):
+        return self.check_player_won(self.player*-1)
+
+    def utility(self):
+        if self.check_player_won(1):
+            return 1
+        elif self.check_player_won(-1):
+            return -1
+        else:
+            return 0
+
+    def check_player_won(self, player: int):
+        # We check if the previous player has won
+        def is_player(arr: np.ndarray):
+            return (arr[0] if player == 1 else arr[1]) == 1
+
+        # Check if both sides of the players path is populated with one of its own pieces
+        vectors = (self.board[0], self.board[-1]) if player == 1 else ([x[0] for x in self.board], [x[-1] for x in self.board])
+        for check in [len(list(filter(is_player, vector))) != 0 for vector in vectors]:
+            if not check:
+                return False
+
+        # TODO DFS search to find path from one side to the other
+
+        is_final_state: Callable[[tuple[int, int]], bool] = (lambda x: x[0] == 3) if player == 1 else (lambda x: x[1] == 3)
+
+        frontier: LifoQueue[tuple[int, int]] = LifoQueue()
+        if player == 1:
+            for i in range(self.size):
+                if is_player(self.board[0][i]):
+                    frontier.put((0,i))
+        else:
+            for i in range(self.size):
+                if is_player(self.board[i][0]):
+                    frontier.put((i,0))
+        visited = list()
+
+        while not frontier.empty():
+            next = frontier.get()
+            visited.append(next)
+            if is_final_state(next):
+                return True
+            neighbors = list(filter(lambda x: is_player(self.board[x[0]][x[1]]), self.neighbors(next)))
+            neighbors = list(filter(lambda x: x not in visited, neighbors))
+            for neighbor in neighbors:
+                frontier.put(neighbor)
+        return False
+
+
+    def neighbors(self, coordinate: tuple[int,int]):
+        (x,y) = coordinate
+        neighbors = [(x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1)]
+        neighbors = list(filter(lambda n: n[0] >= 0 and n[0] < self.size and n[1] >= 0 and n[1] < self.size, neighbors))
+        return neighbors
 
     def __str__(self):
         hexarray = []
@@ -83,8 +139,30 @@ class HexWorld(Game):
         return hexarray
 
 if __name__ == "__main__":
-    world = HexWorld(10)
-    w2 = world.apply("0202").apply("0200").apply("0302")
 
-    for world in w2.get_children_states():
-        print(world[1])
+
+    # No-win scenario
+    world = HexWorld(4)
+    world = HexWorld(4).apply("0000").apply("0303").apply("0100")
+    print(world)
+    print(f"Current player: {world.player}")
+    print(f"Utility: {world.utility()}\n")
+
+
+    # P1 win scenario
+    world = HexWorld(4)
+    world = HexWorld(4).apply("0000").apply("0303").apply("0100").apply("0201").apply("0200").apply("0202").apply("0300")
+    print(world)
+    print(f"Current player: {world.player}")
+    print(f"Player 1 won: {world.is_final_state()}")
+    print(f"Utility: {world.utility()}\n")
+
+
+    # P2 win scenario
+    world = HexWorld(4)
+    world = HexWorld(4).apply("0000").apply("0300").apply("0100").apply("0201").apply("0200").apply("0202").apply("0101").apply("0203")
+    print(world)
+    print(f"Current player: {world.player}")
+    print(f"Player 2 won: {world.is_final_state()}")
+    print(f"Utility: {world.utility()}\n")
+
