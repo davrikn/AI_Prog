b6a87e733a04cc1aefbe53dd8cf4442bbe4e0834
Andreas <60923027+starheim98@users.noreply.github.com>
2023-04-18 14:59:22 +0200

    stuff

--- configs.py
+++ configs.py
@@ -18,14 +18,14 @@ save_data = False
 num_episodes = 1000
 num_rollouts = 250
 decay_rate = 1
-epsilon = 0.25
+epsilon = 0.15
 deepflush = False
 
 # ANET parameters
 loss_function = nn.MSELoss()  # SGDLoss
 optimizer = torch.optim.Adam  # SGD
 input_variables = 100
-learning_rate = 0.002
+learning_rate = 0.001
 num_hidden_layers = 50
 activation_function = 'RELU'
 
