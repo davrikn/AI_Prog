da6d763cb34b3c5cdd35584e69df82e2650b795e
Andreas <60923027+starheim98@users.noreply.github.com>
2023-04-28 14:17:45 +0200

    some decent models, however OHT not working

--- OHT/play_online.py
+++ OHT/play_online.py
@@ -1,25 +1,44 @@
 # Import and initialize your own actor
+import numpy as np
+
 import configs
 from hex.hexAgent import HexAgent
 from ActorClient import ActorClient
 from hex.hexModel import HexModel
 
 actor = HexAgent(model=HexModel(boardsize=7,
-                                snapshotdir=configs.model_dir + 'hex_size_7_checkpoint_0.pt'), name="OHT")
+                                snapshotdir=configs.model_dir + 'hex_size_7_checkpoint_125.pt'), name="OHT")
 
 
 # Import and override the `handle_get_action` hook in ActorClient
 
 
 class MyClient(ActorClient):
-    player_to_move = 0
+    starting_player = 0
 
     def handle_game_start(self, start_player):
-        MyClient.player_to_move = start_player
+        if start_player == 1:
+            MyClient.starting_player = 1
+        else:
+            MyClient.starting_player = -1
 
     def handle_get_action(self, state):
-        row, col = actor.get_action(state, MyClient.player_to_move)  # Your logic
+        row, col = actor.get_action(state, MyClient.starting_player)  # Your logic
         return row, col
+
+    def handle_game_over(self, winner, end_state):
+        player_id = end_state.pop(0)
+        end_state = np.reshape(end_state, (configs.OHT_size, configs.OHT_size))
+        if winner == 1:
+            print("We won (1)")
+        else:
+            print("OHT won (2)")
+        if MyClient.starting_player == 1:
+            print("We started")
+        else:
+            print("OHT started")
+        print("END STATE:")
+        print(end_state, "\n\n")
 
 
 # Initialize and run your overridden client when the script is executed
